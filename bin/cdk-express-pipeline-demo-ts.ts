65fc5d6c765255d1f9b4a667f49175a411dfefcc
rehanvdm <rehan.vdm4@gmail.com>
2024-08-05 06:20:18 +0200

    fix: stack envs

--- bin/cdk-express-pipeline-demo-ts.ts
+++ bin/cdk-express-pipeline-demo-ts.ts
@@ -8,6 +8,10 @@ import {App} from "aws-cdk-lib";
 import {CdkExpressPipeline} from "cdk-express-pipeline";
 
 const app = new App();
+const stackEnv = {
+  account: "581184285249",
+  region: "eu-west-1"
+};
 const expressPipeline = new CdkExpressPipeline();
 
 // === Wave 1 ===
@@ -15,15 +19,15 @@ const wave1 = expressPipeline.addWave('Wave1');
 // --- Wave 1, Stage 1---
 const wave1Stage1 = wave1.addStage('Stage1');
 
-const stackA = new StackA(app, 'StackA', wave1Stage1);
-const stackB = new StackB(app, 'StackB', wave1Stage1);
+const stackA = new StackA(app, 'StackA', wave1Stage1, {env: stackEnv});
+const stackB = new StackB(app, 'StackB', wave1Stage1, {env: stackEnv});
 stackB.addExpressDependency(stackA);
 
 // === Wave 2 ===
 const wave2 = expressPipeline.addWave('Wave2');
 // --- Wave 2, Stage 1---
 const wave2Stage1 = wave2.addStage('Stage1');
-new StackC(app, 'StackC', wave2Stage1);
+new StackC(app, 'StackC', wave2Stage1, {env: stackEnv});
 
 expressPipeline.synth([
   wave1,
